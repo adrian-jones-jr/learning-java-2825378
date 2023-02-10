f1e521ddc6eeed7e21b1090a67d6f58541e5372d
adrian jones <adrian.jones.jr@protonmail.com>
2023-02-10 02:35:44 -0600

    Create Main.java
    Added example usage of if-else and while control flow statements

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Pick a number between 1 amd 10");
+        Scanner scanner = new Scanner(System.in);
+
+        int inputtedNum = scanner.nextInt();
+
+        if (inputtedNum < 5) {
+            System.out.println("Enjoy the good luck a friend brings you");
+        } else {
+            System.out.println("Your shoe selection will make you very happy today");
+        }
+
+        boolean isOnRepeat = true;
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeat? If so, answer yes");
+            String userInput = scanner.next();
+
+            if(userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
+
+            System.out.println("Playing next song");
+        }
+    }
+}
