019d182301c2ac6a8c54f3db64daf34cb4de7c66
adrian jones <adrian.jones.jr@protonmail.com>
2023-02-10 00:27:33 -0600

    Create Main.java
    Created example class file using int, double, char, and boolean data types

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int studentAge = 27;
+        double studentGPA = 3.4;
+        char studentFirstInitial = 'A';
+        char studentLastInitial = 'J';
+        boolean hasPerfectAttendance = false;
+
+        System.out.println((studentAge));
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
